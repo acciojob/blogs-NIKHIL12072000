9f3008585be3db60b59ed50f0d530c4c1f9593cc
Nikhil <dachepallinikhil@gmail.com>
2023-03-29 14:40:01 +0530

    Tasks Completed

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -1,7 +1,6 @@
 package com.driver.services;
 
 import com.driver.models.Blog;
-import com.driver.models.Image;
 import com.driver.models.User;
 import com.driver.repositories.BlogRepository;
 import com.driver.repositories.ImageRepository;
@@ -23,11 +22,14 @@ public class BlogService {
 
     public Blog createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
-
+        User user=userRepository1.findById(userId).get();
+        Blog blog=new Blog(title,content,user);
+        blogRepository1.save(blog);
+        return blog;
     }
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
-
+        blogRepository1.deleteById(blogId);
     }
 }
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 @Service
 public class ImageService {
-
     @Autowired
     BlogRepository blogRepository2;
     @Autowired
@@ -17,15 +16,27 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
-
+        Blog blog=blogRepository2.findById(blogId).get();
+        Image image=new Image(description,dimensions,blog);
+        blog.getImageList().add(image);
+        return image;
     }
 
     public void deleteImage(Integer id){
-
+        imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
-
+        Image image=imageRepository2.findById(id).get();
+        String imageDimensions= image.getDimensions();
+        String[] imgDim=imageDimensions.split("X");
+        String[] scrDim=screenDimensions.split("X");
+        int l=Integer.parseInt(""+scrDim[0]);
+        int b=Integer.parseInt(""+scrDim[1]);
+        int l1=Integer.parseInt(""+imgDim[0]);
+        int b1=Integer.parseInt(""+imgDim[1]);
+        int count=(l/l1)*(b/b1);
+        return count;
     }
 }
